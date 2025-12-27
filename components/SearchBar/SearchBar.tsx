f302349beb5a9643d5d5e2e8894d773a24a72c26
Cia <53003092+ciamac-da@users.noreply.github.com>
2025-12-27 09:38:55 +0100

    header height fixed & some spacing adjustments

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '@/components/SearchBar/SearchBar.module.scss';
 import { useI18n } from '@/lib/i18n';
 
